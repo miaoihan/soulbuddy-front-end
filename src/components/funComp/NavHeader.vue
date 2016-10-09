7e2edffbcb66a4381149d962c1d62d1d9d74ba48
zhanghan <izhanghan@outlook.com>
2016-10-10 01:27:27 +0800

    导航

--- src/components/funComp/NavHeader.vue
+++ src/components/funComp/NavHeader.vue
@@ -5,11 +5,21 @@
 	  <span class="nav-left">
 	  	<i class="iconfont" v-if="title=='新灵伙伴'">&#xe600;</i><!-- 搜索图标 -->
 	  </span>
+	  <!-- 导航右部分	 -->
 	  <span class="nav-right">
 	  	<a class="btn-nav" v-if="title=='新灵伙伴'"
 	  			v-link="{path: '/ask'}">提问</a>
 	  </span>
-	  <h1 class="nav-title">{{ title }}</h1>
+	  <h1 class="nav-title" v-if="title">{{ title }}</h1>
+
+		<div class="tag-wrap" v-if="iscst">
+			<span class="btn-left" :class=" identity===1 ? 'active' : '' " 
+						@click="click(1)"
+						>咨询师</span>
+			<span class="btn-right" :class="{ active: identity === 2 }"
+						@click="click(2)"
+						>经验达人</span>
+		</div>
 	</header>
 
 </template>
@@ -24,6 +34,15 @@
 			fixed:{
 				type: Boolean,
 				default: false
+			},
+			iscst:{ type: Boolean },
+			identity: { type: Number, default: 1 }
+		},
+
+		methods:{
+			click(i){
+				console.log(this.identity)
+				this.identity = i
 			}
 		},
 
@@ -71,4 +90,25 @@
 	position:absolute;
 	left:0.75rem
 	color #fff	
+	
+.tag-wrapper
+	text-align: center
+	span
+		display inline-block
+		margin-top 7px
+		height 1.5rem
+		line-height: 1.5rem
+		width 4.0rem
+		border 1px solid #fff
+		color #fff
+		font-size: 14px
+	.btn-left
+		border-radius 6px 0 0 6px
+		margin-right: -4px
+	.btn-right
+		border-radius 0px 6px 6px 0px
+	.active
+		background-color: #FFF
+		color: $ztc
+
 </style>
--- src/pages/consultant/Consultant.vue
+++ src/pages/consultant/Consultant.vue
@@ -1,6 +1,6 @@
 <template>
 <!-- 咨询师 -->
-	<nav-header></nav-header>
+	<nav-header :identity.sync="identity"></nav-header>
   <div>
 	  <cst-list :data = "userList1" v-if="identity===1"></cst-list>
 	  <kol-list :data = "userList2" v-if="identity===2"></kol-list>
@@ -18,34 +18,19 @@
 	  	return{
 	  		token: '',
 	  		domain: 'http://xinling.songtaxihuan.com',
-	  		identity: 1, //0普通用户1心理咨询师2经验答人
+	  		identity: -1, //0普通用户1心理咨询师2经验答人
 	  		userList1: [],
 	  		userList2: [],
 	  	}
 	  },
 	  ready(){
-	  	// 异步获取token
-	  	$.ajax({
-          url: "http://xinling.songtaxihuan.com/test/test?uid=3",
-          type:'GET', 
-          dataType: 'json',
-          cache: true,
-          async:false,
-          success: function(data) {
-            this.token = data.data
-            // console.log( typeof this.token);	
-          }.bind(this),
-          error: function(xhr, status, err) {
-          }.bind(this)
-        });
-
 	  	$.ajax({
           url: this.domain +'/user/get_user_list',
           type:'POST', 
           dataType: 'json',
           cache: true,
           data:{
-          	token: this.token,
+          	token: global.token,
           	page: 1,
           	identity: 1
           },
@@ -62,7 +47,7 @@
           dataType: 'json',
           cache: true,
           data:{
-          	token: this.token,
+          	token: global.token,
           	page: 1,
           	identity: 2
           },
@@ -73,44 +58,6 @@
           error: function(xhr, status, err) {
           }.bind(this)
         });
-	  },
-	  props:{
-	  	data: {
-	  		type: Array,
-		  	default(){
-		  		return[
-		  			{
-		  				name: '陈雪晴',
-		  				desc: '国家二级心理咨询师',
-		  				label: [
-		  				{
-		  					name: '自杀倾向',
-		  				},
-		  				{
-		  					name: '自残',
-		  				},
-		  				],
-		  				num: 89
-		  			},
-		  			{
-		  				name: '王大伟',
-		  				desc: '国家二级心理咨询师',
-		  				label: [{
-		  					name: '自杀倾向'
-		  				}],
-		  				num: 66
-		  			},
-		  			{
-		  				name: '李梓嫚',
-		  				desc: '国家二级心理咨询师',
-		  				label: [{
-		  					name: '自杀倾向'
-		  				}],
-		  				num: 59
-		  			},
-		  		]
-		  	}
-	  	}
 	  },
 	  
   }
